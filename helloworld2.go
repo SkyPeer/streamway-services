537fb86995ec8bc64655e866c90e519c2654af5d
Oleg Selivantsev <o.selivancev@gmail.com>
2025-08-01 17:01:18 +0400

    go services upd

--- helloworld2.go
+++ helloworld2.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	// Note: Also remove the 'os' import.
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
 
-func main() {
-    fmt.Println("Hello, World!")
+const keyServerAddr = "serverAddr"
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "Hello, HTTP!\n")
 }
