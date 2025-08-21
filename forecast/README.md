134172b9c617661182bb7eb05f0d960a39e9fc0c
Oleg Selivantsev <o.selivancev@gmail.com>
2025-08-22 00:38:04 +0400

    forecast service init

---
+++ forecast/README.md
@@ -0,0 +1,10 @@
+## Forecast FastAPI Service
+
+### Quickstart
+
+- Install deps:
+  
+- Run the app:
+  
+- Health check:
+  
---
+++ forecast/app/main.py
@@ -0,0 +1,14 @@
+# uvicorn main: app - -reload - -port 8500
+from fastapi import FastAPI
+from api.v1.endpoints import health
+app = FastAPI(title="Forecast Service")
+
+
+
+# Routers
+app.include_router(health.router, prefix="/api/v1")
+# app = FastAPI()
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("app.main:app", host="localhost", port=8400, reload=True)
